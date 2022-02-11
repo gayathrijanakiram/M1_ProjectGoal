53ec5ace5b7410e028985d23afef908a53253ec7
gayathrijanakiram <gayathrinalina25@gmail.com>
2022-02-11 19:15:40 +0530

    cmt

---
+++ 3_Implementation/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    float total_amount,transfer,deposite,withdraw,check_balance;
+    int pin,password,user_input;
+    printf("enter your password to enter in your account");
+    scanf("%d",&password);
+    printf("enter amount to create account");
+    scanf("%f",&password);
+    printf("Enter 1 for check balance\n.Enter 2 for deposite\n.Enter 3 for withdraw\n.Enter 4 for transfer\n.");
+    scanf("%d",&user_input);
+    printf("Enter pin\n");
+    scanf("%d",&pin);
+    switch(use_input)
+    {
+        case 1 :
+           break;
+        case 2 :
+           break;
+        case 3 :
+           break;
+        case 4 :
+           break;
+              
+    }
+}
