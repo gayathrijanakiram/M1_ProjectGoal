9266fc07361a5d57b9a768c6f5a934f72ed0f841
gayathrijanakiram <gayathrinalina25@gmail.com>
2022-02-12 15:16:09 +0530

    commit

---
+++ 3_Implementation/func.h
@@ -0,0 +1,10 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+void userLogin();
+void mainMenu();
+void checkBalanceAmount(float balance);
+float cashDeposit(float balance);
+float cashWithdraw(float balance);
+void systemExit();
+void errorMessage();
+#endif
--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,25 +1,84 @@
-#include<stdio.h>
-int main(){
-    float total_amount,transfer,deposite,withdraw,check_balance;
-    int pin,password,user_input;
-    printf("enter your password to enter in your account");
-    scanf("%d",&password);
-    printf("enter amount to create account");
-    scanf("%f",&password);
-    printf("Enter 1 for check balance\n.Enter 2 for deposite\n.Enter 3 for withdraw\n.Enter 4 for transfer\n.");
-    scanf("%d",&user_input);
-    printf("Enter pin\n");
-    scanf("%d",&pin);
-    switch(use_input)
-    {
-        case 1 :
-           break;
-        case 2 :
-           break;
-        case 3 :
-           break;
-        case 4 :
-           break;
-              
-    }
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"func.c"
+
+
+//Main Code
+int main()
+ {
+    //Local Declarations
+    int option;
+    float balance = 20000.00;
+    int choose;
+    int choice;
+    
+    bool again = true;
+    
+    // insert code here...
+    printf("Do you wear your mask and sanitized before entering ATM?\n");
+    printf("< 1 > Yes\n");
+    printf("< 2 > No\n");
+    scanf("%d",&choice);
+    if(choice==1){
+        
+    while (again) {
+    mainMenu();
+    
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("Your Selection:\t");
+    scanf("%d", &option);
+    
+    
+        switch (option) {
+            case 1:
+            	system("CLS");
+                checkBalanceAmount(balance);
+                break;
+            case 2:
+            	system("CLS");
+                balance = cashDeposit(balance);
+                break;
+            case 3:
+            	system("CLS");
+                balance = cashWithdraw(balance);
+                break;
+            
+            case 4:
+            	system("CLS");
+                systemExit();
+                return 0;
+                
+            default:
+                errorMessage();
+                break;
+        }
+        
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("Do you wish to have another transaction:\n");
+        printf("< 1 > Yes\n");
+        printf("< 2 > No\n");
+        scanf("%d", &choose);
+        
+        system("CLS");
+        
+        
+        
+        if (choose == 2) {
+            again = false;
+            systemExit();
+            
+        }
+       
 }
+
+    
+    }else{
+     printf("***Please wear your mask and sanitize to avail the ATM transaction***");   
+    }
+    return 0;
+}//main code
+
+
+
